42feca92024fc420c8b4f595b9ce54a1fb125b34
Euripides <lee82511p@gmail.com>
2023-05-07 16:02:22 +0800

    All

---
+++ GYM.php
@@ -0,0 +1,4 @@
+<?php
+$url = "https://gist.githubusercontent.com/EuripidesLee/bb447ac85fc0996081b7b675767ced10/raw/53dc371b03d1cb6b6000e48b9eb063a9fe4b210c/GYM.json";
+echo file_get_contents($url);
+?>
